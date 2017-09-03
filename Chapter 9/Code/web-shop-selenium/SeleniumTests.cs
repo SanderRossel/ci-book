aedcf1eff2a1e43a687de6bbbb18077d7c5257ce
Sander Rossel <sander.rossel@gmail.com>
2017-09-03 12:34:59 +0200

    Fixed tests in chapter 9
    Restored the Selenium tests to what they're supposed to be at this stage.

--- Chapter 9/Code/web-shop-selenium/SeleniumTests.cs
+++ Chapter 9/Code/web-shop-selenium/SeleniumTests.cs
@@ -1,21 +1,21 @@
-using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
-using System.Linq;
-using System.Threading;
-using System.Net.Http;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.AspNetCore.Hosting;
-using web_shop;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using Xunit;
+using web_shop;
 
 namespace web_shop_selenium
 {
     public class SeleniumTests : IDisposable
     {
         private readonly IWebDriver driver = new ChromeDriver();
-        
+
         private readonly TestServer server;
         private readonly HttpClient client;
 
@@ -41,12 +41,12 @@ namespace web_shop_selenium
             Assert.Equal("nowrap", elem.GetCssValue("white-space"));
         }
 
-        /*[Fact]
+        [Fact]
         public void ShouldNavigateToTheClickedProduct_Test()
         {
-            driver.Navigate().GoToUrl("http://localhost:63700");
+            driver.Navigate().GoToUrl("http://localhost:5000");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-            
+
             var product = driver.FindElements(By.CssSelector("h3"))[1];
             product.Click();
 
@@ -57,11 +57,11 @@ namespace web_shop_selenium
         [Fact]
         public void ShouldSearchForAllProductsContainingFanta_Test()
         {
-            driver.Navigate().GoToUrl("http://localhost:63700");
+            driver.Navigate().GoToUrl("http://localhost:5000");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-            
+
             var input = driver.FindElement(By.CssSelector("[ng-model=query]"));
-            input.SendKeys("fanta");            
+            input.SendKeys("fanta");
             var searchBtn = driver.FindElement(By.CssSelector("#search-btn"));
             searchBtn.Click();
             var results = driver.FindElements(By.CssSelector(".thumbnail"));
@@ -72,44 +72,44 @@ namespace web_shop_selenium
                 var caption = elem.FindElement(By.CssSelector("h3")).Text;
                 return caption.ToLowerInvariant().Contains("fanta");
             });
-            
+
             Assert.True(allHaveFanta, "Not all found results contain the text 'fanta'.");
         }
 
         [Fact]
         public void ShouldRemoveAnItemFromTheShoppingCart_Test()
         {
-            driver.Navigate().GoToUrl("http://localhost:63700/ShoppingCart");
+            driver.Navigate().GoToUrl("http://localhost:5000/ShoppingCart");
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
-            
+
             // redirect            
             var username = driver.FindElement(By.CssSelector("input[ng-model=username]"));
             var password = driver.FindElement(By.CssSelector("input[type=password]"));
-            
+
             username.SendKeys("user");
             password.SendKeys("password");
-            
+
             var submit = driver.FindElement(By.CssSelector("input[type=submit]"));
             submit.Click();
 
             // another redirect
-            driver.Navigate().GoToUrl("http://localhost:63700");
-            
+            driver.Navigate().GoToUrl("http://localhost:5000");
+
             var buttons = driver.FindElements(By.CssSelector(".btn.btn-primary"));
-            
+
             buttons[0].Click();
             driver.SwitchTo().Alert().Accept();
-            
+
             buttons[1].Click();
             driver.SwitchTo().Alert().Accept();
-            
-            driver.Navigate().GoToUrl("http://localhost:63700/ShoppingCart");
+
+            driver.Navigate().GoToUrl("http://localhost:5000/ShoppingCart");
 
             var items = driver.FindElements(By.CssSelector(".thumbnail"));
             var count = items.Count;
             var caption = items[0].FindElement(By.CssSelector("h3")).Text;
             items[0].FindElement(By.CssSelector("button")).Click();
-            
+
             // Like with the JavaScript tests, just wait for a second...
             Thread.Sleep(1000);
             var newItems = driver.FindElements(By.CssSelector(".thumbnail"));
@@ -119,7 +119,7 @@ namespace web_shop_selenium
             driver.Navigate().Refresh();
             var afterRefresh = driver.FindElements(By.CssSelector(".thumbnail"));
             Assert.Equal(count - 1, afterRefresh.Count);
-        }*/
+        }
 
         #region IDisposable Support
         private bool disposedValue = false;
@@ -137,7 +137,7 @@ namespace web_shop_selenium
                 disposedValue = true;
             }
         }
-        
+
         void IDisposable.Dispose()
         {
             Dispose(true);
