acf4888d0d565484cf821c13fd817da444fb19b0
Sander Rossel <sander.rossel@gmail.com>
2017-09-06 17:10:01 +0200

    Fixed SQL script in chapter 9
    Fixed a , to a ; in a SQL script in chapter 9.

--- Chapter 9/Code/web-shop/sql/04 - CREATE TABLE public.shopping_cart.sql
+++ Chapter 9/Code/web-shop/sql/04 - CREATE TABLE public.shopping_cart.sql
@@ -3,7 +3,7 @@ CREATE TABLE public.shopping_cart
    id bigserial NOT NULL,
    user_id integer NOT NULL,
    product_id integer NOT NULL,
-   "number" integer NOT NULL DEFAULT 1,
+   "number" integer NOT NULL DEFAULT 1;
    PRIMARY KEY (id),
    CONSTRAINT fk_shopping_cart_user FOREIGN KEY (user_id)
       REFERENCES public."user" (id) MATCH SIMPLE
